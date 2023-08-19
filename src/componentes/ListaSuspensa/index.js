c1d923becf14a57b4646037f53836c6b92a74181
“Kedna <kednacleiasousa@hotmail.com>
2023-08-19 11:20:19 -0300

    continuação lista suspensa

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,14 +1,19 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
+	console.log(props.itens)
 	return (
-		<div>
+		<div className='lista__suspensa'>
 			<label>{props.label}</label>
 			<select>
-				<option>???</option>
+				{props.itens.map(item => {
+					return <option key={item}>{item}</option>
+				})} 
 			</select>
 		</div>
 	)
 }
 
 export default ListaSuspensa
+
+// no select pode ser -- {props.itens.map(item => <option>{item}</option>)} -- para escrever menos
